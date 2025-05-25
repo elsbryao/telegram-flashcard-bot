83fa3fa889d3067fbc141c65da18434cc475105f
elsbryao <elsbryao72@gmail.com>
2025-05-25 15:59:33 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def save_and_zip(flashcards, elevenlabs_api_key, voice_id):
 def extract_flashcards(prompt, api_key):
     client = OpenAI(api_key=api_key)
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.4
     )
